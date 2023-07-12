ea81066e520f2950c7e73745d0417e5a0b725fb9
Alex <https://github.com/AlexVelcheor13/Andersen.git>
2023-07-12 17:48:25 +0300

    init commit

---
+++ src/main/java/mb/test/demo/OrgTestApplication.java
@@ -0,0 +1,13 @@
+package mb.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mb/test/demo/OrgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
